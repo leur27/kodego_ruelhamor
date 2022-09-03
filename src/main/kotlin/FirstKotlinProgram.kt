249608ca8cd178ef7d4b8334dfcabd33e2d06326
ruel hamor <noyee82@gmail.com>
2022-09-03 09:15:00 +0800

    commit some program

--- src/main/kotlin/FirstKotlinProgram.kt
+++ src/main/kotlin/FirstKotlinProgram.kt
@@ -3,7 +3,27 @@ fun main(){
     Author: Ruel Hamor
     Date Created: 9/1/22
     Filename: FirstKotlinProgram.kt
-         */
-    print("Ruel")
-    print(" Hamor")
+    git remote add origin
+    */
+
+    var firstName : String = "Ruel"
+    var age = 21
+    var grade = 75.00
+    var section = 'A'
+    var isEnrolled = true
+
+    println("Enter your first number: ")
+    var firstNumber : Int = readln().toInt()
+
+    println("Enter your second number: ")
+    var secondNumber : Int = readln().toInt()
+
+    var sum : Int = firstNumber + secondNumber
+    println("The sum is $sum")
+
+    //println("My Name is $firstName")
+    //println("I am $age years old")
+    //println("I got $grade")
+    //println("I belong to section $section")
+    //println("Yes it is $isEnrolled I am currently enrolled")
 }
