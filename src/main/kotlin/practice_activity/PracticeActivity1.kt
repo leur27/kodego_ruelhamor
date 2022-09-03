0ec436ad346a859ae52f62263f0993a4a46e4800
ruel hamor <noyee82@gmail.com>
2022-09-03 16:43:03 +0800

    commit1

---
+++ src/main/kotlin/practice_activity/PracticeActivity1.kt
@@ -0,0 +1,16 @@
+
+
+fun main(){
+
+    println("Enter your first name: ")
+    var firstName : String = readln().toString()
+
+    println("Enter your middle name/initial: ")
+    var middleName : String = readln().toString()
+
+    println("Enter your last name: ")
+    var lastName : String = readln().toString()
+    
+    println("Your full name is $firstName $middleName $lastName ")
+
+}
