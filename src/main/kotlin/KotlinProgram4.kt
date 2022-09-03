42fe63adda9c91e73b3b1827a25b8229fcb506fd
ruel hamor <noyee82@gmail.com>
2022-09-03 15:01:12 +0800

    commit1

---
+++ src/main/kotlin/KotlinProgram4.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var number : Int = 10
+
+    if(number < 10){
+        println("Hello")
+    }
+
+    var grades : Int = 75
+    if (grades >= 75) {
+        println("You passed!")
+    }
+
+    //range
+    var idNumber : Int = 1
+    if ((idNumber >=1)&&(idNumber <=3)){
+        println("valid id number")
+    }
+
+    println("Line ends")
+}
