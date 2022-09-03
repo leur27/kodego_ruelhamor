d438015fd7a0c189a382fe8b2c09907cdc93af54
ruel hamor <noyee82@gmail.com>
2022-09-03 15:23:16 +0800

    commit3

--- src/main/kotlin/KotlinProgram4.kt
+++ src/main/kotlin/KotlinProgram4.kt
@@ -1,16 +1,25 @@
 fun main(){
 
-    var grades : Int = 74
-    if (grades >= 75) {
-        println("You passed!")
-    }else{
-        println("you failed")
+    var grade : Int = 95
+
+
+    if ((grade >= 96)&&(grade <=100)) {
+        println("1.00")
+    }else if((grade >= 86)&&(grade <=95)){
+        println("1.75")
+    }else if((grade >= 76)&&(grade <=85)){
+        println("2.25")
+    }else if(grade < 75){
+        println("5.0")
+    }else {
+        println("Invalid grade")
     }
 
+
     //range
-    var idNumber : Int = 1
-    if ((idNumber >=1)&&(idNumber <=3)){
-        println("valid id number")
-    }
-    println("Line ends")
+//    var idNumber : Int = 1
+//    if ((idNumber >=1)&&(idNumber <=3)){
+//        println("valid id number")
+//    }
+//    println("Line ends")
 }
