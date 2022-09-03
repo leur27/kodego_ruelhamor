db8e12d0dd10a0f71c60c05fb9073db476da16f6
ruel hamor <noyee82@gmail.com>
2022-09-03 09:49:54 +0800

    commit1

---
+++ src/main/kotlin/KotlinProgram2.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var string1 : String = "This is my String"
+    var string2 : String = "This is my String 2"
+    //string character
+    println(string1[5])
+    //string length
+    println(string1.length)
+    println(string2.length)
+    //string equality
+    println(string1 == string2)
+    //string uppercase/lowercase
+    println(string1.lowercase())
+    println(string1.uppercase())
+    //startswith/endswith
+    //println(string2.startsWith(prefix:"this"))
+    //concatinate
+    //println(string1+" "+string2)
+    //substring
+    println(string1.substring(0,4))
+    //replace
+    println(string1.replace(oldValue = "This", newValue = "That"))
+}
