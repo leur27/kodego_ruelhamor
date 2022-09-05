5e5a170bca433b5f857f83ecf7a6374c423a0522
ruel hamor <noyee82@gmail.com>
2022-09-05 19:36:03 +0800

    commit1

---
+++ src/main/kotlin/KotlinProgram8.kt
@@ -0,0 +1,13 @@
+fun main(){
+    //while loop
+    //initialization (init)
+    var ctr : Int = 1
+    //condition
+    while (ctr <=5 ){
+        println("Hello World! $ctr")
+        //update
+        ctr++
+    }
+
+
+}
