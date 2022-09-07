f44afeaf766b7f93305705f0618df750aa71ee82
ruel hamor <noyee82@gmail.com>
2022-09-07 20:47:45 +0800

    commit2

--- src/main/kotlin/KotlinProgram16.kt
+++ src/main/kotlin/KotlinProgram16.kt
@@ -1,15 +1,28 @@
 fun main(){
     var names = ArrayList<String>()
-    names.add("John")
-    names.add("Peter")
+//    names.add("John")
+//    names.add("Peter")
+//
+//    //add elements
+//    println(names[0])
+//    println(names[1])
+//
+//    //remove elements
+//    names.removeAt(0)//Peter -> index 0
+//
+//    //display[0]
+//    println(names[0])
 
-    //add elements
-    println(names[0])
-    println(names[1])
+    //add names to arraylist
+    names.add("Mary")
+    names.add("Jenny")
+    names.add("Mark")
+    names.add("Paul")
+    names.add("Bryan")
+    names.add("Roel")
 
-    //remove elements
-    names.removeAt(0)//Peter -> index 0
-
-    //display[0]
-    println(names[0])
+    //traverse
+    for(value in names){
+        print(" $value ")
+    }
 }
