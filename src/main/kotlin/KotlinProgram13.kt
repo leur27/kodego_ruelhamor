e702bd324b26071aa1970afb13b8bdeb244a4c8f
ruel hamor <noyee82@gmail.com>
2022-09-07 19:49:15 +0800

    commit1

---
+++ src/main/kotlin/KotlinProgram13.kt
@@ -0,0 +1,12 @@
+fun main(){
+    //implicit dec         0      1    2
+    // array size = 3
+    var names = arrayOf("John","Paul","Mark")
+    println(names[0])
+
+    //explicit
+    var numbers = arrayOf<Int>(2,3,4,5,110)
+
+    //array size
+    println(numbers.size)
+}
