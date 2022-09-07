accfae389393c8639f8ffda528744cb7570775e9
ruel hamor <noyee82@gmail.com>
2022-09-07 20:41:39 +0800

    commit1

---
+++ src/main/kotlin/KotlinProgram16.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var names = ArrayList<String>()
+    names.add("John")
+    names.add("Peter")
+
+    //add elements
+    println(names[0])
+    println(names[1])
+
+    //remove elements
+    names.removeAt(0)//Peter -> index 0
+
+    //display[0]
+    println(names[0])
+}
