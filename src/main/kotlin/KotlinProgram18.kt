e7a873e0460ab283bcca61b814bc672513c65c3b
ruel hamor <noyee82@gmail.com>
2022-09-09 20:23:02 +0800

    commit2

--- src/main/kotlin/KotlinProgram18.kt
+++ src/main/kotlin/KotlinProgram18.kt
@@ -1,4 +1,24 @@
 fun main(){
+    //declaring map
     var map = mapOf(1 to "Peter", 2 to "Paul", 3 to "Mary")
-    println(map)
+
+//    println(map)
+//    println(map[2])
+//    println(map.get(2))
+
+    //traversing keys
+    for (key in map.keys){
+        print(key)
+    }
+
+    //traversing values
+    for (value in map.values){
+        print(value)
+    }
+
+    //mutable
+    var map2 = mutableMapOf<Int,String>()
+    map2.put(2, "John")
+    map2.remove(2)
+    println(map2)
 }
