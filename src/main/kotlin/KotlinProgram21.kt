8a6aef164b12a6319949cf10e105c45f49c0f852
ruel hamor <noyee82@gmail.com>
2022-09-10 11:33:55 +0800

    commit2

--- src/main/kotlin/KotlinProgram21.kt
+++ src/main/kotlin/KotlinProgram21.kt
@@ -1,16 +1,32 @@
 fun main(){
 
-    var myObject = MyClass("Hello World")
-    var myObject2 = MyClass("Hi Earth")
+    //instantiation - process of creating an object
+//    var myObject = MyClass("Hello World")
+//    var myObject2 = MyClass("Hi Earth")
+//
+    var myGenericObject = MyGenericClass("words")
+    var myGenericObject2 = MyGenericClass(3.1416)
+}
 
+class  MyGenericClass<T>(content : T){
+    init {
+        println(content.toString().length)
+    }
 }
 
 class MyClass(word : String){
     init{
         println(word)
+        println(word.length)
     }
 }
 
+class MyClass2(value : Int){
+    init{
+        println(value)
+        println(value.toString().length)
+    }
+}
 /*
 class -> objects
 objects -> variables
