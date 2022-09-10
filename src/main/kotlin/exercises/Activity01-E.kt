af6508b00a1ad2ca1d50b2ee94a6f30291413a16
ruel hamor <noyee82@gmail.com>
2022-09-10 09:03:17 +0800

    commit2

--- src/main/kotlin/exercises/Activity01-E.kt
+++ src/main/kotlin/exercises/Activity01-E.kt
@@ -12,7 +12,7 @@ fun  main(){
     println("\nType a word/words: ")
     var line2 : String = readln()
 
-    var combi = ""
+    var combi : String = ""
 
     for (i in 0 until line1.length) {
         for (j in 0 until line2.length) {
@@ -23,9 +23,9 @@ fun  main(){
     }
 
     for (i in 0 until combi.length) {
-        val charToRemove = combi[i] + ""
-        line1 = line1.replace(charToRemove, "")
-        line2 = line2.replace(charToRemove, "")
+        var remove = combi[i] + ""
+        line1 = line1.replace(remove, "")
+        line2 = line2.replace(remove, "")
     }
     println("\nUnique characters are: $line1$line2")
 
