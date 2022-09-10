f55638cc3e2fe7e8dde8e12f743bda7f65424c96
ruel hamor <noyee82@gmail.com>
2022-09-10 11:24:10 +0800

    commit1

---
+++ src/main/kotlin/KotlinProgram21.kt
@@ -0,0 +1,24 @@
+fun main(){
+
+    var myObject = MyClass("Hello World")
+    var myObject2 = MyClass("Hi Earth")
+
+}
+
+class MyClass(word : String){
+    init{
+        println(word)
+    }
+}
+
+/*
+class -> objects
+objects -> variables
+        -> functions(process)
+
+(class) uppercase -> first letter
+samples:
+String
+Int
+ */
+
