8ae9c096d18178cfff74ecee2d866561754f6a5d
ruel hamor <noyee82@gmail.com>
2022-09-13 06:46:08 +0800

    commit3

--- src/main/kotlin/practice_activity/PracticeActivity5.kt
+++ src/main/kotlin/practice_activity/PracticeActivity5.kt
@@ -1,23 +1,25 @@
 /*
 Author: Ruel Hamor
 Date Created: 9/9/22
-Filename: ExerciseNo4.kt
-Exercise No.: 4
+Filename: PracticeActivity5.kt
+Exercise No.: 5
 */
 
 fun  main(){
 
     var numbers = arrayOf<Int> (98, 76, 54, 32, 10)
+
     var ctr : Int = 0
 
     while(ctr < numbers.size){
-        println("Enter new value: ")
+        println("Enter number ${ctr+1}: ")
         numbers[ctr] = readln().toInt()
         numbers.set(1,numbers[ctr])
 
         ctr++
     }
 
+    println("Previous List [8, 76, 54, 32, 10] \nchanged to New List:")
     println(numbers.contentToString())
 
 }
