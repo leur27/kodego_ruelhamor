cd74806a7972a6a1db501bb15c202b5f8230581a
ruel hamor <noyee82@gmail.com>
2022-09-12 16:31:58 +0800

    commit2

--- src/main/kotlin/exercises/Activity01-F.kt
+++ src/main/kotlin/exercises/Activity01-F.kt
@@ -1,9 +1,17 @@
+/*
+Author: Ruel Hamor
+Date Created: 9/10/22
+Filename: Activity01-F.kt
+Exercise No.: 6
+*/
+
 fun main(){
 
     println("Type a word: ")
     var str1 : String = readln()
     var str2 : String = str1.reversed()
 
+    //checking
     if (str1 == str2) {
         println("The word $str1 that you entered is a Palindrome")
     }else{
