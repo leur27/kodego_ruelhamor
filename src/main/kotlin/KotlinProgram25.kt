08218c9e2332ede938e842de2f04b0150fb93871
ruel hamor <noyee82@gmail.com>
2022-09-13 21:00:34 +0800

    commit1

---
+++ src/main/kotlin/KotlinProgram25.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+    var sum : (Int,Int) -> Int = { num1, num2 ->
+        num1 + num2
+    }
+    println(sum(2,3))
+}
+
+fun sum2(num1:Int, num2:Int): Int{
+    return num1 + num2
+}
