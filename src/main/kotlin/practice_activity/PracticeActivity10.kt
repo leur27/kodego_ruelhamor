eaaf925cf7fb6f4e2fdafef83a9e68f025c773c8
ruel hamor <noyee82@gmail.com>
2022-09-20 16:33:16 +0800

    commit5

--- src/main/kotlin/practice_activity/PracticeActivity10.kt
+++ src/main/kotlin/practice_activity/PracticeActivity10.kt
@@ -2,7 +2,7 @@ package practice_activity
 
 fun main(){
     //instantiation
-    val cellPhone = SmartPhone(15.50, 12)
+    val cellPhone = SmartPhone(15.50, 12.0)
     var condition : Boolean = true
     lateinit var newPercent2: Number
     lateinit var newPercent: Number
@@ -22,7 +22,8 @@ fun main(){
     while(condition){
         println("--------------------------------------------")
         println("---Select an option---")
-        println("[1] Reload Sim\n[2] Battery Recharge\n[3] Write Messages\n[4] Balance Inquiry\n[5] Make Calls\n[6] Power Status\n[0] Exit App\nEnter here:")
+        println("[1] Reload Sim\n[2] Battery Recharge\n[3] Write Messages\n[4] Balance Inquiry")
+        println("[5] Make Calls\n[6] Power Status\n[0] Exit App\nEnter here:")
         println("--------------------------------------------")
         select = readln().toInt()
         if (select == 1){
@@ -30,11 +31,12 @@ fun main(){
             newAmount = readln().toDouble()
             cellPhone.buyLoad(newAmount)
         }else if (select == 2){
-            println("[ Assumption: 1% power added per 2 minutes of charging & reduce by 1% every 2 minutes used  ]")
+            println("[ Assumption: 2% power added per minute of charging & reduce by 1% every 2 minutes used  ]")
             println("Would you like to charge the battery? Y/N: ")
             answer = readln()
             if (answer=="Y"||answer=="y") {
-                println("Percentage needed to full: ${100 - cellPhone.batCapacity}% \nCharging time needed to full: ${(100 - cellPhone.batCapacity) / 2} minutes")
+                println("Percentage needed to full: ${100 - cellPhone.batCapacity}% ")
+                println("Charging time needed to full: ${(100 - cellPhone.batCapacity)/2} minutes")
                 println("Choose: \n[1] Charged already \n[0] Return to Menu")
                 input = readln().toInt()
                 if (input == 1) {
@@ -46,14 +48,14 @@ fun main(){
                     continue
                 }
             }else{
-                println("Percentage left to use: ${cellPhone.batCapacity}%\nTime left to shut down: ${(cellPhone.batCapacity)/2}")
-                println("Choose: \n[1] Dont Charge \n[0] Return to Menu")
+                println("Percentage left to use: ${cellPhone.batCapacity}%\nTime left to shut down: ${(cellPhone.batCapacity)*12}")
+                println("Choose: \n[1] Use Phone \n[0] Return to Menu")
                 input2  = readln().toInt()
                 if (input2 == 1){
                     println("Actual used without charging (minutes): ")
                     minutes = readln().toInt()
-                    newPercent2 = minutes*2
-                    cellPhone.powerUpdate(percent2 = newPercent2)
+                    newPercent2 = minutes/2
+                    cellPhone.chargeBat(percent = newPercent2)
                 }else{
                     continue
                 }
@@ -64,19 +66,22 @@ fun main(){
             println("Type your message: ")
             message = readln()
             cellPhone.sendText(message)
-            println("Received by: ${recipient.uppercase()}")
+            println("Sent to: ${recipient.uppercase()}")
         }else if (select==4){
             cellPhone.checkBalance(balance = cellPhone.load)
         }else if (select==5){
             if (cellPhone.load > 0.0){
                 println("How long your call will take? (minutes):")
+//                call = (readln().toInt())
                 cellPhone.makeCalls(readln().toDouble())
-                println("[1] Call\n[0] Cancel")
+                println("[1] Called\n[0] Cancel")
                 input3 = readln().toInt()
                 if (input3==1){
                     println("Actual minutes of call made:")
-                    call = (readln().toInt())
-                    println("${cellPhone.load - call} minutes of call left")
+//                    cellPhone.makeCalls(readln().toDouble())
+                    call = (readln().toInt()).times(8).minus(1)
+                    println("${cellPhone.load-call}0 load left")
+                    cellPhone.powerUpdate(percent2 = cellPhone.load-call)
                 }else{
                     continue
                 }
@@ -92,7 +97,7 @@ fun main(){
     }
 }
 
-class SmartPhone(var load : Double, var batCapacity : Int){
+class SmartPhone(var load : Double, var batCapacity : Double){
 
 
     init {
@@ -104,22 +109,31 @@ class SmartPhone(var load : Double, var batCapacity : Int){
     fun buyLoad(amount:Double) {                                                        //does not accept zero or negative values
         if (amount > 0){
             load += amount
+            println("Successfully load")
         }else{
             println("You entered an invalid amount")
         }
     }
     fun chargeBat(percent:Int){                                                         //(percent)-> does not accept zero or negative values
 
-        if (percent > 0){
-            batCapacity += percent
+        batCapacity += percent
             if (batCapacity>=100){
                 println("Battery Full")
+            }else if(batCapacity<=0){
+                println("Empty Battery ")
             }else{
                 println("Battery Charged ")
             }
-        }else{
-            println("Battery is empty!")
-        }
+    //        if (percent > 0){
+//            batCapacity += percent
+//            if (batCapacity>=100){
+//                println("Battery Full")
+//            }else{
+//                println("Battery Charged ")
+//            }
+//        }else{
+//            println("Battery is empty!")
+//        }
     }
     fun sendText(message:String){                                                       //-> display message sent
         if (load>0){
@@ -141,20 +155,18 @@ class SmartPhone(var load : Double, var batCapacity : Int){
     }
     fun makeCalls(minutes:Double){                                                       //(minutes)-> 8 load per minute, will not push through if no sufficient load
         val loadCall = minutes*8
-
-        if (load == loadCall){
-            println("You only have a minute of call")
-        }else if (load < loadCall){
-            println("Insufficient load!")
+        if (load >= loadCall){
+            println("You can make the call")
+            8*(load)--
         }else{
-            println("${load/8} minutes of call available")
+            println("Insufficient load!")
         }
     }
-    fun powerUpdate(percent2:Int){
+    fun powerUpdate(percent2:Double){
            if (percent2==batCapacity){
-               println("${batCapacity}% remaining: shutting down")
+               println("${batCapacity}% remaining battery life")
            }else if (percent2<batCapacity){
-               println("${batCapacity}% remaining: ${batCapacity/2} minutes to use")
+               println("${batCapacity}% remaining: ${batCapacity*2} minutes to use")
            }else{
                println("Dead Battery!!!")
            }
