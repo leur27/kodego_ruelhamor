3f717bfa8fd6af064a38ead918cf1c45d651a9df
ruel hamor <noyee82@gmail.com>
2022-09-24 11:04:35 +0800

    commit1

---
+++ src/main/kotlin/com/oop/Main.kt
@@ -0,0 +1,16 @@
+package com.oop
+
+fun main(){
+//    var p1 = Person()
+//    var ageFromPerson = p1.upAge()
+//    var joePerson = Person()
+//    joePerson.name = "Peter"
+//    println(joePerson.name)
+//    joePerson.age = -1
+//    joePerson.change(24)
+//    println("Hello World")
+
+    var joeStudent = Student()
+    println(joeStudent.name)
+    joeStudent.age = -1
+}
