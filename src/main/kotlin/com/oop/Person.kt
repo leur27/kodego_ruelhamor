b6a7800ed0ae0592a4f2b9e4dcf7d52a7f4dd58a
ruel hamor <noyee82@gmail.com>
2022-09-24 11:05:28 +0800

    commit1

--- src/main/kotlin/com/oop/Person.kt
+++ src/main/kotlin/com/oop/Person.kt
@@ -1,4 +1,39 @@
 package com.oop
 
-class Person {
+//Abstraction
+//Encapsulation
+open class Person{
+    var name: Any = "John"
+        get() {return field}
+        set(value){
+            field = value
+        }
+    var age: Int = 23
+        get() {return field}
+        set(value) {
+            if (value < 1){
+                println("Invalid Age")
+            }else{
+                field = value
+            }
+        }
+
+    init{
+        println("Object Created!")
+        println("name is $name, age is $age")
+    }
+
+    fun upAge():Int{
+        age++
+        return age
+    }
+
+//    fun setAge(name:Int):Int{
+//
+//    }
+
+    fun change(age:Int){
+        println(this.age)
+    }
+
 }
