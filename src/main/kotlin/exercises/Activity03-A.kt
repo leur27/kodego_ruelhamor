760ec6bef29cb700fd97813c7930746c0d73914f
ruel hamor <noyee82@gmail.com>
2022-09-25 08:24:10 +0800

    commit1

---
+++ src/main/kotlin/exercises/Activity03-A.kt
@@ -0,0 +1,44 @@
+fun main() {
+
+    print("---------------------------------------------------------------------------------------")
+    println("\nThis machine solves the total amount of unlimited input amount and divided by a number ")
+    println("---------------------------------------------------------------------------------------")
+
+    //program starts here / declared variables
+    var condi: Boolean = true
+    var total = 0.0
+    lateinit var answer: String
+    lateinit var input: Number
+    lateinit var div: Number
+
+    while (condi) {                                              //loop statement / condition is true
+        print("Would you like to continue? Y/N: ")              //call to action answerable only by Y / N
+        answer = readln()
+        if (answer == "Y" || answer == "y") {
+            print("Enter an amount:")
+            input = readln().toDouble()
+            total += input
+        } else if (answer == "N" || answer == "n") {
+            println("\nTotal amount is $total")
+            condi = false
+        }
+    }
+    //input a number here to divide the total amount //try-catch block
+//    try {
+        println("\nDivide the total by how many? ")
+        div = readln().toInt()
+        val quotient: Number = (total / div)
+        println("\nThe result is $quotient")
+        println("\nTransaction completed")
+//    } catch (e:Exeption) {
+//        println(e)
+//        println(" ")
+//    }
+
+    println("THANK YOU!")
+}
+
+private operator fun Number.div(div: Int): Number {
+return div
+}
+
