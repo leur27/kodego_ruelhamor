1ddb64c3a53b16154936ee664786a5093f3544af
ruel hamor <noyee82@gmail.com>
2022-09-28 10:39:47 +0800

    commit2

--- src/main/kotlin/com/oop/Main.kt
+++ src/main/kotlin/com/oop/Main.kt
@@ -1,16 +1,7 @@
 package com.oop
-
 fun main(){
-//    var p1 = Person()
-//    var ageFromPerson = p1.upAge()
-//    var joePerson = Person()
-//    joePerson.name = "Peter"
-//    println(joePerson.name)
-//    joePerson.age = -1
-//    joePerson.change(24)
-//    println("Hello World")
-
-    var joeStudent = Student()
-    println(joeStudent.name)
-    joeStudent.age = -1
+    var manager = Manager()
+    manager.computeBonus()
+    var staff = Staff()
+    staff.computeBonus()
 }
