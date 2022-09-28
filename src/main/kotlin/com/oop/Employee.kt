a1aca9ffd5e860d05cc850f916e38dcb354d575e
ruel hamor <noyee82@gmail.com>
2022-09-28 10:40:32 +0800

    commit1

--- src/main/kotlin/com/oop/Employee.kt
+++ src/main/kotlin/com/oop/Employee.kt
@@ -1,4 +1,15 @@
 package com.oop
 
-class Employee {
+ open class Employee {
+    var salary : Double = 3000.0
+    var bonus : Double = 1000.0
+
+     init{
+         println("New employee created")
+     }
+
+     open fun computeBonus(){
+         println("Bonus is $bonus")
+     }
+
 }
