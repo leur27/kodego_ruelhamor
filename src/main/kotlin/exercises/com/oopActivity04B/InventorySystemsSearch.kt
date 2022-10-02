c5cbc2615f4384cbf8600e6cf42e6ba1872790ff
ruel hamor <noyee82@gmail.com>
2022-10-02 17:19:17 +0800

    commit1

--- src/main/kotlin/exercises/com/oopActivity04B/InventorySystemsSearch.kt
+++ src/main/kotlin/exercises/com/oopActivity04B/InventorySystemsSearch.kt
@@ -1,4 +1,46 @@
 package exercises.com.oopActivity04B
 
-class InventorySystemsSearch {
+class InventorySystemsSearch: InventorySystem() {
+
+    override fun searchByItemNumber(itemNumber:Int):Boolean{
+        super.searchByItemNumber(itemNumber)
+        var combinedMap = foodItem + toiletriesItem + nonFoodItem
+        var combinedMap2 = foodItem2 + toiletriesItem2 + nonFoodItem2
+        var combinedMap3 = foodItem3 + toiletriesItem3 + nonFoodItem3
+        var combinedMap4 = foodItem4 + toiletriesItem4 + nonFoodItem4
+        var combinedMap5 = foodItem5 + toiletriesItem5 + nonFoodItem5
+        println("Searching details for item number $itemNumber")
+        println("Product:   ${(combinedMap[itemNumber])}")
+        println("Category:  ${(combinedMap2[itemNumber])}")
+        println("Inventory: ${(combinedMap3[itemNumber])}")
+        println("Unit:      ${(combinedMap4[itemNumber])}")
+        println("Price:     ${(combinedMap5[itemNumber])}")
+        return  true
+    }
+
+    override fun searchByProductName(productName:String):Boolean{
+        var combinedMap = foodItem + toiletriesItem + nonFoodItem
+        var combinedMap2 = foodItem2 + toiletriesItem2 + nonFoodItem2
+        var combinedMap3 = foodItem3 + toiletriesItem3 + nonFoodItem3
+        var combinedMap4 = foodItem4 + toiletriesItem4 + nonFoodItem4
+        var combinedMap5 = foodItem5 + toiletriesItem5 + nonFoodItem5
+        val filteredMap = combinedMap.filterValues { it == productName }
+        for(value in filteredMap.values) {
+            if (value == productName) {
+                filteredMap.forEach { (k, v) ->
+                    println("Item No.: $k   Product Name: $v")
+                    for (key in filteredMap.keys) {
+                        val itemNumber = key
+                        println("Category:  ${(combinedMap2[itemNumber])}")
+                        println("Inventory: ${(combinedMap3[itemNumber])}")
+                        println("Unit:      ${(combinedMap4[itemNumber])}")
+                        println("Price:     ${(combinedMap5[itemNumber])}")
+                    }
+                }
+            }else{
+                println("Item is not listed")
+            }
+        }
+        return true
+    }
 }
