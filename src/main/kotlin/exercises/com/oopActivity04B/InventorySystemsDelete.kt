ec2b48945251d2f84966b3f66f62638614b7d123
ruel hamor <noyee82@gmail.com>
2022-10-02 17:17:59 +0800

    commit1

--- src/main/kotlin/exercises/com/oopActivity04B/InventorySystemsDelete.kt
+++ src/main/kotlin/exercises/com/oopActivity04B/InventorySystemsDelete.kt
@@ -1,4 +1,23 @@
 package exercises.com.oopActivity04B
 
-class InventorySystemsDelete {
+class InventorySystemsDelete: InventorySystem() {
+
+    override fun deleteInventory(itemNumber: Int, productName: String, category: String, inventory: Double, itemUnit: String, itemPrice: Double):Boolean{
+        foodItem.remove(itemNumber, productName)
+        foodItem2.remove(itemNumber, category)
+        foodItem3.remove(itemNumber, inventory)
+        foodItem4.remove(itemNumber, itemUnit)
+        foodItem5.remove(itemNumber, itemPrice)
+        toiletriesItem.remove(itemNumber, productName)
+        toiletriesItem2.remove(itemNumber, category)
+        toiletriesItem3.remove(itemNumber, inventory)
+        toiletriesItem4.remove(itemNumber, itemUnit)
+        toiletriesItem5.remove(itemNumber, itemPrice)
+        nonFoodItem.remove(itemNumber, productName)
+        nonFoodItem2.remove(itemNumber, category)
+        nonFoodItem3.remove(itemNumber, inventory)
+        nonFoodItem4.remove(itemNumber, itemUnit)
+        nonFoodItem5.remove(itemNumber, itemPrice)
+        return  true
+    }
 }
