12b6cbce9738b362913b72afc9ae5299b8287c03
ruel hamor <noyee82@gmail.com>
2022-10-02 17:17:37 +0800

    commit1

--- src/main/kotlin/exercises/com/oopActivity04B/InventorySystemsAdd.kt
+++ src/main/kotlin/exercises/com/oopActivity04B/InventorySystemsAdd.kt
@@ -1,4 +1,32 @@
 package exercises.com.oopActivity04B
 
-class InventorySystemsAdd {
+class InventorySystemsAdd: InventorySystem() {
+
+    override fun addInventoryItems(itemNumber: Int, productName: String, category: String, inventory: Double, itemUnit: String, itemPrice: Double):Boolean{
+        super.addInventoryItems(itemNumber, productName, category, inventory, itemUnit, itemPrice)
+        foodItem.put(key = itemNumber, value = productName)
+        foodItem2.put(key = itemNumber, value = category)
+        foodItem3.put(key = itemNumber, value = inventory)
+        foodItem4.put(key = itemNumber, value = itemUnit)
+        foodItem5.put(key = itemNumber, value = itemPrice)
+        return  true
+    }
+
+    fun addInventoryToiletries(itemNumber: Int, productName: String, category: String, inventory: Double, itemUnit: String, itemPrice: Double):Boolean{
+        toiletriesItem.put(key = itemNumber, value = productName)
+        toiletriesItem2.put(key = itemNumber, value = category)
+        toiletriesItem3.put(key = itemNumber, value = inventory)
+        toiletriesItem4.put(key = itemNumber, value = itemUnit)
+        toiletriesItem5.put(key = itemNumber, value = itemPrice)
+        return  true
+    }
+
+    fun addInventoryNonFood(itemNumber: Int, productName: String, category: String, inventory: Double, itemUnit: String, itemPrice: Double):Boolean{
+        nonFoodItem.put(key = itemNumber, value = productName)
+        nonFoodItem2.put(key = itemNumber, value = category)
+        nonFoodItem3.put(key = itemNumber, value = inventory)
+        nonFoodItem4.put(key = itemNumber, value = itemUnit)
+        nonFoodItem5.put(key = itemNumber, value = itemPrice)
+        return  true
+    }
 }
