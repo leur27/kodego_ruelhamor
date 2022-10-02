e0be88dc493dd5ef8095d6263f35cb6cd9973bd1
ruel hamor <noyee82@gmail.com>
2022-10-02 17:18:24 +0800

    commit1

--- src/main/kotlin/exercises/com/oopActivity04B/InventorySystemsDisplay.kt
+++ src/main/kotlin/exercises/com/oopActivity04B/InventorySystemsDisplay.kt
@@ -1,8 +1,9 @@
 package exercises.com.oopActivity04B
 
-class InventorySystemsSearch: InventorySystem() {
+class InventorySystemsDisplay: InventorySystem() {
 
-    fun displayInventoryAll():Boolean{
+    override fun displayInventoryAll():Boolean{
+        super.displayInventoryAll()
         var combinedMap = foodItem + toiletriesItem + nonFoodItem
         for (key in foodItem.keys) {
             combinedMap += foodItem
@@ -18,6 +19,30 @@ class InventorySystemsSearch: InventorySystem() {
             println("Item No.: $key   Product Name: ${(sortedMap[key]).toString().uppercase()}")
         }
         println("\nThis directory contains ${(sortedMap.size)} items ")
+        return true
+    }
+
+    override fun displayInventoryFood():Boolean{
+        for (key in foodItem.keys) {
+            println("Item No.: $key   Product Name: ${(foodItem[key]).toString().uppercase()}")
+        }
+        println("\nThis directory contains ${(foodItem.size)} items ")
+        return true
+    }
+
+    override fun displayInventoryToiletries(): Boolean {
+        for (key in toiletriesItem.keys) {
+            println("Item No.: $key   Product Name: ${(toiletriesItem[key]).toString().uppercase()}")
+        }
+        println("\nThis directory contains ${(toiletriesItem.size)} items ")
+        return true
+    }
+
+    override fun displayInventoryNonFood():Boolean{
+        for (key in nonFoodItem.keys) {
+            println("Item No.: $key   Product Name: ${(nonFoodItem[key]).toString().uppercase()}")
+        }
+        println("\nThis directory contains ${(nonFoodItem.size)} items ")
         return true
     }
 
