935eefb5cdb0f52b8783aa59bfb4d12aa74a0fcd
ruel hamor <noyee82@gmail.com>
2022-10-08 15:00:25 +0800

    commit2

--- src/main/kotlin/com/oop/Main.kt
+++ src/main/kotlin/com/oop/Main.kt
@@ -1,7 +1,10 @@
 package com.oop
 fun main(){
-    var manager = Manager()
-    manager.computeBonus()
-    var staff = Staff()
-    staff.computeBonus()
+//    var manager = Manager()
+//    manager.computeBonus()
+//    var staff = Staff()
+//    staff.computeBonus()
+
+//    var personObject = Person()
+//    personObject.displayName()
 }
