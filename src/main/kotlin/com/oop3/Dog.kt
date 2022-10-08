bd8a39716f3c2078740920a662b2b56bf3572a31
ruel hamor <noyee82@gmail.com>
2022-10-08 14:59:32 +0800

    commit2

--- src/main/kotlin/com/oop3/Dog.kt
+++ src/main/kotlin/com/oop3/Dog.kt
@@ -1,4 +1,19 @@
 package com.oop3
 
-class Dog {
+class Dog:Animal,Pet, Mammal() {
+    override fun walk() {
+        println("Dog walks")
+    }
+
+    override fun makeSound() {
+        println("Aw aw")
+    }
+
+    override fun groom() {
+        println("Dog can be groomed")
+    }
+
+    override fun feed() {
+        println("Dog feeds")
+    }
 }
