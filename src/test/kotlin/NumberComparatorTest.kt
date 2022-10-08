06de621a09d9eb9e6d3e2bb06bf9f9e80c1497f8
ruel hamor <noyee82@gmail.com>
2022-10-08 14:54:08 +0800

    commit2

--- src/test/kotlin/NumberComparatorTest.kt
+++ src/test/kotlin/NumberComparatorTest.kt
@@ -1,3 +1,29 @@
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-internal class NumberComparatorTest
+class NumberComparatorTest{
+    //test cases
+
+    @Test
+    fun fiveThreeeOneEnteredReturnFive(){
+//        var numComparator = NumberComparator()
+//        assertEquals(5, numComparator.getMaxNumber(5,3,1))
+    }
+
+    @Test
+    fun oneFiveTwoEnteredReturnFive(){
+//        var numComparator = NumberComparator()
+//        assertEquals(5, numComparator.getMaxNumber(1,5,2))
+    }
+
+    @Test
+    fun twoFiveSevenEnteredReturnSeven(){
+//        var numComparator = NumberComparator()
+//        assertEquals(7, numComparator.getMaxNumber(2,5,7))
+    }
+
+
+
+
+
+}
